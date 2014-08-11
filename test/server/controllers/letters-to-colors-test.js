b33f38be57363d63af94aaf959b08a3a322ebcdd
Dylan Smith <dylantsmith@gmail.com>
2014-08-11 16:49:45 -0500

    Add a letters-to-colors controller test
    Add a simple test to help demonstrate the unit tests on the server side.

---
+++ test/server/controllers/letters-to-colors-test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var lettersToColors = require("../../../controllers/letters-to-colors");
+
+describe("letters-to-colors controller", function() {
+
+    it("should exist", function() {
+        expect(lettersToColors).toBeDefined();
+    });
+
+});
