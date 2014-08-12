a3a90cab834d5df0fca87744db88e0bc62392a32
Dylan Smith <dylantsmith@gmail.com>
2014-08-12 14:28:13 -0500

    Expand on API test, add rewire
    Using rewire, expand on the API test to verify the text is sent to the
    colors service, the return of which is sent to the user.

--- test/server/controllers/letters-to-colors-test.js
+++ test/server/controllers/letters-to-colors-test.js
@@ -1,11 +1,59 @@
 "use strict";
 
-var lettersToColors = require("../../../controllers/letters-to-colors");
+var rewire = require("rewire"),
+    lettersToColors = rewire("../../../controllers/letters-to-colors");
 
 describe("letters-to-colors controller", function() {
+    var generateColorsText, COLORS;
+
+    COLORS = "red and blue";
+
+    beforeEach(function() {
+        lettersToColors.__set__("colorGenerator", {
+            generateColors: function(text) {
+                generateColorsText = text;
+                return COLORS;
+            }
+        });
+    });
 
     it("should exist", function() {
         expect(lettersToColors).toBeDefined();
+    });
+
+    it("should export as a function", function() {
+        expect(typeof lettersToColors).toEqual("function");
+    });
+
+    it("should define a POST function for /api/letters-to-colors", function() {
+        var TEXT, req, res, colors, app, postURI;
+
+        TEXT = "Hello";
+
+        req = {
+            body: {
+                text: TEXT
+            }
+        };
+
+        res = {
+            send: function(obj) {
+                colors = obj.colors;
+            }
+        };
+
+        app = {
+            post: function(uri, callback) {
+                postURI = uri;
+                callback(req, res);
+            }
+        };
+
+        lettersToColors(app);
+
+        expect(postURI).toEqual("/api/letters-to-colors");
+        expect(generateColorsText).toEqual(TEXT);
+        expect(colors).toEqual(COLORS);
     });
 
 });
